a808ff15119b149a5ff3536f8d987f0df8bb2bd5
pawga <ivannikov.s@gmail.com>
2019-12-29 16:31:31 +0300

    01

---
+++ app/src/main/java/com/pawga/myapplication01/data/model/NoteEntity.kt
@@ -0,0 +1,10 @@
+package com.pawga.myapplication01.data.model
+
+/**
+ * Created by pawga on 29.12.19 16:17
+ */
+
+//@Entity(tableName = "notes")
+//data class NoteEntity(
+//    @PrimaryKey(autoGenerate = true) val id: Long,
+//    @ColumnInfo(name = "note_text") val noteText: String)
